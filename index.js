cc1d34f30b6e39c7e378a5857c716e3a9cbc063a
amirhosseinbidokhti <amir.bidokhti99@gmail.com>
2024-04-11 03:39:47 +0330

    useless comment

--- index.js
+++ index.js
@@ -30,37 +30,3 @@ async function main() {
 }
 
 main();
-
-
-
-
-
-
-
-
-
-// const db = await JSONFilePreset('reportsDB.json', {})
-// const disclosedReports = db.data
-
-// let id = "Z2lkOi8vaGFja2Vyb25lL0NvbXBsZXRlSGFja3Rpdml0eVJlcG9ydEluZGV4LzIzMzQ0MjA"
-
-
-// let {TELEGRAM_CHANNEL_ID, TELEGRAM_BOT_TOKEN} = process.env
-
-
-// console.log("TELEGRAM_CHANNEL_ID", TELEGRAM_CHANNEL_ID);
-// console.log("TELEGRAM_BOT_TOKEN", TELEGRAM_BOT_TOKEN);
-
-//console.log(disclosedReports.at(0));
-//console.log(disclosedReports.find(report => report.id = id))
-//let myMessage = disclosedReports.find(report => report.id = id);
-
-//console.log("myMessage", myMessage);
-
-
-//sendNotification(JSON.stringify(myMessage), TELEGRAM_CHANNEL_ID, TELEGRAM_BOT_TOKEN)
-
-
-// Add verbose and logging function 
-// Add switch to determine the cronjob
-// 
