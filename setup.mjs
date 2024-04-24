d42c4b92e778a77eb1a6c78f79425aed84752f12
amirhosseinbidokhti <amir.bidokhti99@gmail.com>
2024-04-25 00:46:48 +0330

    fix: 8h cron default

--- setup.mjs
+++ setup.mjs
@@ -7,4 +7,4 @@ console.log("INSTALLING THE PACKAGES:");
 await $`npm i`;
 // Setup the pm2 with default command
 console.log("STARTING PM2:");
-await $`pm2 start "node index.js -c 1m" --watch`;
+await $`pm2 start "node index.js -c 8h" --watch`;
